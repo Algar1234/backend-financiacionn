53d2c94b24c1cb609a5d7364d442ddf58c0123d7
Algar1234 <114458635+Algar1234@users.noreply.github.com>
2023-01-21 23:28:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 var mongoose= require('mongoose');
 const app = require('./app');
-const port= 3700;
+const port= 7209;
+var MONGOPORT=7209;
+var MONGOPASSWORD='vyDf77FkXARVKecXLw3Y';
+var MOUSEONGR= 'mongo';
+var MONGOHOST='containers-us-west-89.railway.app';
 mongoose.set("strictQuery", false);
 mongoose.Promise= global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/backend-financiacion')
+//mongoose.connect('mongodb://localhost:27017/backend-financiacion')
+mongoose.connect('mongodb://:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}')
 .then(()=>{
     console.log('Conexion a labase de datos establecida satisfactoriamente')
      
-    app.listen(port,()=>{//metodo de express
+    app.listen(MONGOPORT,()=>{//metodo de express
         console.log('Servidor corriendo correctamente en la url: localhost:3700')
     })
 })
