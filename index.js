ebeb8276ffd7dde8f701a9d1b17acaf4285ac5a0
Algar1234 <114458635+Algar1234@users.noreply.github.com>
2023-01-22 18:10:02 -0400

    ecec
    dedceec

--- index.js
+++ index.js
@@ -2,16 +2,41 @@
 
 var mongoose= require('mongoose');
 const app = require('./app');
+const uri = process.env.url;
+mongoose.set("strictQuery", false);
+mongoose.Promise= global.Promise;
+mongoose.connect(uri)
+.then(()=>{
+    console.log('Conexion a labase de datos establecida satisfactoriamente')
+     
+    app.listen(3700,()=>{//metodo de express
+        console.log('Servidor corriendo correctamente en la url: localhost:3700')
+    })
+})
+.catch(err=> console.log(err));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+/*
+var mongoose= require('mongoose');
+const app = require('./app');
 const port= 7209;
-var MONGOPORT=7209;
+var MONGOPORT='7209';
 var MONGOPASSWORD='vyDf77FkXARVKecXLw3Y';
-var MOUSEONGR= 'mongo';
+var MOUSEONGR= 'Mongo';
 var MONGOHOST='containers-us-west-89.railway.app';
 mongoose.set("strictQuery", false);
 mongoose.Promise= global.Promise;
-
+//mongoose.connect('mongodb://:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}')
 //mongoose.connect('mongodb://localhost:27017/backend-financiacion')
-mongoose.connect('mongodb://:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}')
+mongoose.connect('mongodb+srv://anel710:v2qw*_J*8Ne@UX#@backend-financiacion.sblllud.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>{
     console.log('Conexion a labase de datos establecida satisfactoriamente')
      
@@ -21,3 +46,4 @@ mongoose.connect('mongodb://:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT
 })
 .catch(err=> console.log(err));
 
+*/
